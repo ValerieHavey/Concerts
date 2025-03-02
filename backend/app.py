10ffdff9f05cc06d99277592f95074dd5d003e5a
Valeriehavey-ga <valeriehavey@gmail.com>
2025-03-02 15:11:30 -0800

    DB Date code change

--- backend/app.py
+++ backend/app.py
@@ -156,7 +156,7 @@ def create_tables():
     try:
         connection = get_db_connection()
         cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cursor.execute('CREATE TABLE if not exists concerts (id serial Primary key, headliner text not null, openers text, date DateTime not null, location text not null, notes text, concert_goer integer not null)')
+        cursor.execute('CREATE TABLE if not exists concerts (id serial Primary key, headliner text not null, openers text, date Date not null, location text not null, notes text, concert_goer integer not null)')
         cursor.execute('CREATE TABLE if not exists users (id serial Primary Key, user text not null, password text not null)')
         connection.commit()
         # concerts = cursor.fetchall()
